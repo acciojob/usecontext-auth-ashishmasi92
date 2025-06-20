d47e9c259d5b036f2e3d27e981ed3fce545ce5fe
ashish <ashishmasih92g@gmail.com>
2025-06-20 20:59:32 +0530

    initialize a assignment

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,10 @@ const App = () => {
     <div>
        <h1>Click on the Checkbox to get Authenticated</h1>
        <p className="authText">
-        {isAuthenticated
-          ? "You are authenticated"
-          : "You are not authenticated"}
-      </p>
+  {isAuthenticated
+    ? "You are now authenticated, you can proceed"
+    : "you are not authenticated"}
+</p>
     <input type="checkbox" name="checkbox" id="AuthToggle" onChange={(e)=>{
       setIsAuthenticated(e.target.checked)
     }} />
